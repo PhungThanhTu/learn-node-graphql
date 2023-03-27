d13b5f329dbe24a690923ba0c6bf1d7bf7596e6d
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-03-28 06:24:50 +0700

    demo simple query

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+    type Query {
+        description: String,
+        price: Float
+    }
+`);
+
+const root = {
+    description: "Basic hat",
+    price: 10.5
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root
+}));
+
+app.listen(4777, () => {
+    console.log("Running GraphQL Server");
+});
+
