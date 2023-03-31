4117d774260d7a21c0db7e1493e322166021bfe4
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-04-01 03:19:07 +0700

    modularized graphql types with graphql tools

---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 'redshoe',
+        description: 'red shoe',
+        price: 42.12
+    },
+    {
+        id: 'bluejean',
+        description: 'Blue Jeans',
+        price: 55.55
+    }
+]
