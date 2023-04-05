e7b2c70c2e9a3ae0dc89098d10da7fe12d84db69
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-04-05 08:20:32 +0700

    mutation

--- products/products.model.js
+++ products/products.model.js
@@ -2,12 +2,14 @@ const products  = [
     {
         id: 'redshoe',
         description: 'red shoe',
-        price: 42.12
+        price: 42.12,
+        reviews: []
     },
     {
         id: 'bluejean',
         description: 'Blue Jeans',
-        price: 55.55
+        price: 55.55,
+        reviews: []
     }
 ]
 
@@ -29,4 +31,16 @@ function getProductById(id) {
     });
 }
 
-module.exports = { getAllProducts, getProductsByPrice, getProductById };
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: []
+    }
+    products.push(newProduct);
+
+    return newProduct;
+}
+
+module.exports = { getAllProducts, getProductsByPrice, getProductById, addNewProduct };
